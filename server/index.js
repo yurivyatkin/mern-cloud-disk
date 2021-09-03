44231336e9b952054df1c56532cb9ff0fe614173
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-09-03 23:17:24 +1200

    [#3] Wire up a simple server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+const config = require('config');
+
+const express = require('express');
+const PORT = config.get('serverPort');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const start = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log('Server started on port', PORT);
+    });
+  } catch (e) {
+    // need to handle errors
+  }
+};
+
+start();
