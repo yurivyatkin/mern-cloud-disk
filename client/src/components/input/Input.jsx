976eb24566cc23985bea5c9859f0378407450430
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-09-05 13:29:38 +1200

    [#7] Make Input component

---
+++ client/src/components/input/Input.jsx
@@ -0,0 +1,14 @@
+import './input.css';
+
+const Input = (props) => {
+  return (
+    <input
+      onChange={(event) => props.setValue(event.target.value)}
+      value={props.value}
+      type={props.type}
+      placeholder={props.placeholder}
+    />
+  );
+};
+
+export default Input;
