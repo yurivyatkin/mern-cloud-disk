443a30fe0b14d27aa273580fd30503bc4af08d17
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-09-07 00:08:53 +1200

    [#9] Create user's folder at the registration time

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,7 +5,11 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 const User = require('../models/User');
+const File = require('../models/File')
+
 const authMiddleware = require('../middleware/auth.middleware');
+
+const fileService = require('../services/fileService')
 
 const router = new Router();
 
@@ -38,6 +42,7 @@ router.post(
       const hashPassword = await bcrypt.hash(password, 8);
       const user = new User({ email, password: hashPassword });
       await user.save();
+      await fileService.createDir(new File({user:user.id, name: ''}))
 
       return res.json({ message: 'User was created' });
     } catch (e) {
