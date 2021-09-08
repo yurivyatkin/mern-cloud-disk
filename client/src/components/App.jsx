7635f6569ece26a4c4882c2af0b74b67879c9557
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-09-08 23:42:07 +1200

    [#10] Show Disk for authenticated users

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Navbar from './navbar/Navbar';
 import Registration from './authorization/Registration';
 import Login from './authorization/Login';
+import Disk from './disk/Disk';
+
 import { auth } from '../actions/user';
 
 import './app.css';
@@ -22,10 +24,16 @@ function App() {
       <div className="app">
         <Navbar />
         <div className="wrap">
-          {!isAuth && (
+          {!isAuth ? (
             <Switch>
               <Route path="/registration" component={Registration} />
               <Route path="/login" component={Login} />
+              <Redirect to="/login" />
+            </Switch>
+          ) : (
+            <Switch>
+              <Route exact path="/" component={Disk} />
+              <Redirect to="/" />
             </Switch>
           )}
         </div>
