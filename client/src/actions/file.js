ba33dfa5a913e665b9018fc77834ab832b89457d
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-09-08 23:32:32 +1200

    [#10] Add file actions

---
+++ client/src/actions/file.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { setFiles } from '../reducers/fileReducer';
+
+export function getFiles(dirId) {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/api/files${dirId ? '?parent=' + dirId : ''}`,
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        }
+      );
+      console.log('getFiles response.data', response.data);
+      dispatch(setFiles(response.data.files));
+    } catch (e) {
+      // alert(e.response.data.message);
+      console.log('actions/file/getFiles', e);
+    }
+  };
+}
