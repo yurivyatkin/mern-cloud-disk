199a7354fdff1787a29645b3894268214238abda
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-09-16 23:15:00 +1200

    [#15] Build the drag area for uploading files

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import FileList from './fileList/FileList';
@@ -13,6 +13,7 @@ const Disk = () => {
   const dispatch = useDispatch();
   const currentDir = useSelector((state) => state.files.currentDir);
   const dirStack = useSelector((state) => state.files.dirStack);
+  const [dragEnter, setDragEnter] = useState(false);
 
   useEffect(() => {
     dispatch(getFiles(currentDir));
@@ -32,8 +33,33 @@ const Disk = () => {
     files.forEach((file) => dispatch(uploadFile(file, currentDir)));
   }
 
-  return (
-    <div className="disk">
+  function dragEnterHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    setDragEnter(true);
+  }
+
+  function dragLeaveHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    setDragEnter(false);
+  }
+
+  function dropHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    let files = [...event.dataTransfer.files];
+    files.forEach((file) => dispatch(uploadFile(file, currentDir)));
+    setDragEnter(false);
+  }
+
+  return !dragEnter ? (
+    <div
+      className="disk"
+      onDragEnter={dragEnterHandler}
+      onDragLeave={dragLeaveHandler}
+      onDragOver={dragEnterHandler}
+    >
       <div className="disk__btns">
         <button className="disk__back" onClick={() => backClickHandler()}>
           Back
@@ -43,7 +69,7 @@ const Disk = () => {
         </button>
         <div className="disk__upload">
           <label htmlFor="disk__upload-input" className="disk__upload-label">
-            Загрузить файл
+            Upload file
           </label>
           <input
             multiple={true}
@@ -56,6 +82,16 @@ const Disk = () => {
       </div>
       <FileList />
       <PopUp />
+    </div>
+  ) : (
+    <div
+      className="drop-area"
+      onDrop={dropHandler}
+      onDragEnter={dragEnterHandler}
+      onDragLeave={dragLeaveHandler}
+      onDragOver={dragEnterHandler}
+    >
+      Drag files here
     </div>
   );
 };
