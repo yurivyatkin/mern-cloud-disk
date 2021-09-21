1acfea8c0158f16e959958a456d74a08d42ef67b
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-09-21 22:48:15 +1200

    [#19] Animate folder opening

--- client/src/components/disk/fileList/FileList.jsx
+++ client/src/components/disk/fileList/FileList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './fileList.css';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { useSelector } from 'react-redux';
+
 import File from './file/File';
+
+import './fileList.css';
 
 const FileList = () => {
   // state.files by default is set to [] and with getFiles is set to data.files from the response
   const fileList = useSelector((state) => state.files.files);
-  const fileElements = fileList.map((file) => (
-    <File key={file.id} file={file} />
-  ));
 
   return (
     <div className="filelist">
@@ -17,7 +17,18 @@ const FileList = () => {
         <div className="filelist__date">Date</div>
         <div className="filelist__size">Size</div>
       </div>
-      {fileElements}
+      <TransitionGroup>
+        {fileList.map((file) => (
+          <CSSTransition
+            key={file._id}
+            timeout={500}
+            classNames={'file'}
+            exit={false}
+          >
+            <File file={file} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
