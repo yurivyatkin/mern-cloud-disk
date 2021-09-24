a415343af9d26f3b267737b4bb1730debeb77dfb
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-09-24 23:21:10 +1200

    [#21] Implement search for files by name

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { logout } from '../../reducers/userReducer';
+import { showLoader } from '../../reducers/appReducer';
+
+import { getFiles, searchFiles } from '../../actions/file';
 
 import Logo from '../../assets/img/navbar-logo.svg';
 
@@ -9,12 +14,47 @@ import './navbar.css';
 
 function Navbar() {
   const isAuth = useSelector((state) => state.user.isAuth);
+  const currentDir = useSelector((state) => state.files.currentDir);
+
   const dispatch = useDispatch();
+
+  const [searchName, setSearchName] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(false);
+
+  function searchChangeHandler(e) {
+    setSearchName(e.target.value);
+    if (searchTimeout !== false) {
+      clearTimeout(searchTimeout);
+    }
+    dispatch(showLoader());
+    if (e.target.value !== '') {
+      setSearchTimeout(
+        setTimeout(
+          (value) => {
+            dispatch(searchFiles(value));
+          },
+          500,
+          e.target.value
+        )
+      );
+    } else {
+      dispatch(getFiles(currentDir));
+    }
+  }
 
   return (
     <div className="navbar">
       <img src={Logo} alt="" className="navbar__logo" />
       <div className="navbar__header">MERN CLOUD</div>
+      {isAuth && (
+        <input
+          value={searchName}
+          onChange={(e) => searchChangeHandler(e)}
+          className="navbar__search"
+          type="text"
+          placeholder="File name..."
+        />
+      )}
       {!isAuth && (
         <div className="navbar__login">
           <NavLink to="/login">Log In</NavLink>
--- server/routes/file.routes.js
+++ server/routes/file.routes.js
@@ -7,6 +7,7 @@ router.post('', authMiddleware, fileController.createDir);
 router.get('', authMiddleware, fileController.getFiles);
 router.post('/upload', authMiddleware, fileController.uploadFile);
 router.get('/download', authMiddleware, fileController.downloadFile);
+router.get('/search', authMiddleware, fileController.searchFile);
 router.delete('/', authMiddleware, fileController.deleteFile);
 
 module.exports = router;
